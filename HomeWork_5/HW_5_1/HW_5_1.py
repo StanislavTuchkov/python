2c18da5bfc9101b9a7dae459348ee188085876a3
StanislavTuchkov <78221479+StanislavTuchkov@users.noreply.github.com>
2021-02-22 13:29:30 +0300

    Delete HomeWork_5 directory

--- HomeWork_5/HW_5_1/HW_5_1.py
+++
@@ -1,4 +0,0 @@
-f = open('file.txt', 'x')
-new_data = input('Enter your message to the future - ')
-f.write(new_data)
-f.close()
--- HomeWork_5/HW_5_5/HW_5_5.py
+++
@@ -1,7 +0,0 @@
-with open('new_file_number.txt', 'w', encoding='utf-8') as numbers:
-    user_numbers = input('Enter few numbers - ')
-    numbers.write(user_numbers)
-with open('new_file_number.txt', 'r', encoding='utf-8') as numbers:
-  a = numbers.readline().split()
-  # readline() приянть данные как строку через запятую! Не мог сразу понять как это сделать
-  print(sum(map(int, a)))
